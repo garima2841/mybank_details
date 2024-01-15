c1e357873af8b6360d163dd1ea7bea1e1cacf389
Garima Singh <garimasingh2841@gmail.com>
2024-01-15 23:53:44 +0000

    First Commit

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -10,6 +10,24 @@ sap.ui.define([
         return Controller.extend("com.sap.mybankdetails.controller.App", {
             onInit: function () {
 
+            },
+            onOpenBankDetails: function () {
+                //create dialog lazy
+                if (!this.moreBankDetails) {
+                    this.moreBankDetails = this.loadFragment(
+                        {
+                            name: "com.sap.mybankdetails.view.fragments.MoreDetails"
+                        }
+                    );
+                }
+                this.moreBankDetails.then(function (oDialog) {
+                    oDialog.open();
+                });
+            },
+            // Assuming this is part of your controller
+            onCloseDialog: function () {
+                this.byId("moreBankDetails").close();
             }
+
         });
     });
